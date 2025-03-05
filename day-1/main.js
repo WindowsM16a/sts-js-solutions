f2da49bdf3b35ca57912a2898cea5372b92521cf
WindowsM16a <wuvershayne@gmail.com>
2025-03-05 12:46:12 +0000

    i think this is the assignment, if i'm right, i'm done, else i'll need to redo this

---
+++ day-1/main.js
@@ -0,0 +1,16 @@
+// declaring different variables with different data types
+
+let firstName = 'Shayne';
+let lastName = 'Wuver';
+let country = 'Ghana';
+let city = 'Accra';
+let age = '19';
+let isMarried = false;
+
+// declaring variables with numerical values
+const gravity = 9.81;
+const boilingPoint = 100;
+const PI = 3.14;
+
+// declaring variables on one line with one keyword
+let name = 'Shayne Wuver', job = 'Student', live = 'Ghana';
