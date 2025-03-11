c0721016c5671f908e6e38db055e2ed37e99d24c
WindowsM16a <wuvershayne@gmail.com>
2025-03-11 16:41:55 +0000

    done with level 3 assignment

--- day-3/level3.js
+++ day-3/level3.js
@@ -1,1 +1,9 @@
 // q1
+const date = new Date();
+let yearDate = date.getFullYear();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+let hour = date.getHours();
+let minutes = date.getMinutes();
+
+console.log(`${year}-${month}-${day} ${hour}:${minutes}`)
