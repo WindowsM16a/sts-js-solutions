31a71ea07bb4072b3442acb6ece88c475074613d
WindowsM16a <wuvershayne@gmail.com>
2025-03-11 16:56:44 +0000

    done with level 1 assignment

--- day-3/level1.js
+++ day-3/level1.js
@@ -1,1 +1,71 @@
 // q1
+let firstName = 'Shayne';
+let lastName = 'Wuver';
+let country = 'Ghana';
+let city = 'Accra';
+let age = 19;
+let isMarried = false;
+let year = '2025';
+
+// q2
+console.log(typeof '10' == 10);
+
+// q3
+console.log(parseInt('9.8') == 10);
+
+// q4
+let myName = 'Shayne Wuver';
+let isHappy = true;
+let goal = 100000000000;
+
+let current = 0;
+let solution;
+let willNotAchieveGoal = false;
+
+// q5
+console.log(4>3);
+console.log(4>=3);
+console.log(4<3);
+console.log(4<=3);
+console.log(4==4);
+console.log(4===4);
+console.log(4!=4);
+console.log(4!==4);
+console.log(4!='4');
+console.log(4=='4');
+console.log(4==='4');
+
+let strA = 'python';
+let strB = 'jargon';
+
+console.log(strA.length != strB.length);
+
+// q6
+console.log(4>3 && 10<12);
+console.log(4>3 && 10>12);
+console.log(4>3 || 10<12);
+console.log(4>3 || 10>12);
+console.log(!(4>3));
+console.log(!(4<3));
+console.log(!(false))
+console.log(!(4>3 && 10<12));
+console.log(!(4>3 && 10>12));
+console.log(!(4===4));
+
+console.log(strA.includes('on') == false, strB.includes('on') == false);
+
+// q7
+let today = new Date();
+console.log(today.getFullYear());
+
+console.log(today.getMonth() + 1)
+
+console.log(today.getDate());
+
+console.log(today.getDay());
+
+console.log(today.getHours());
+
+console.log(today.getMinutes());
+
+console.log(today.getTime());
