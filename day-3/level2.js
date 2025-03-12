2e9ea4b0e4f93afcbc90d1a4b8ae422760f2c4ad
WindowsM16a <wuvershayne@gmail.com>
2025-03-12 22:28:18 +0000

    zerebel no good walahi

--- day-3/level2.js
+++ day-3/level2.js
@@ -1,29 +1,29 @@
-// // q1
-// let base = parseInt(prompt('Enter base:'));
-// let height = parseInt(prompt('Enter height:'));
-// let areaTriangle = (base * height) / 2;
-// console.log(`The area of the triangle is ${areaTriangle}`);
+// q1
+let base = parseInt(prompt('Enter base:'));
+let height = parseInt(prompt('Enter height:'));
+let areaTriangle = (base * height) / 2;
+console.log(`The area of the triangle is ${areaTriangle}`);
 
-// // q2
-// let sideA = parseInt(prompt('Enter side a:'));
-// let sideB = parseInt(prompt('Enter side b:'));
-// let sideC = parseInt(prompt('Enter side c:'));
-// let perimeterTriangle = sideA + sideB + sideC;
-// console.log(`The perimeter of the triangle is ${perimeterTriangle}`);
+// q2
+let sideA = parseInt(prompt('Enter side a:'));
+let sideB = parseInt(prompt('Enter side b:'));
+let sideC = parseInt(prompt('Enter side c:'));
+let perimeterTriangle = sideA + sideB + sideC;
+console.log(`The perimeter of the triangle is ${perimeterTriangle}`);
 
-// // q3
-// let width = parseInt(prompt('Enter width:'));
-// let length = parseInt(prompt('Enter length:'));
-// let areaRectangle = width * length;
-// let perimeterRectangle = 2 * (width + length);
-// console.log(`The area of the rectangle is ${areaRectangle} and the perimeter is ${perimeterRectangle}`)
+// q3
+let width = parseInt(prompt('Enter width:'));
+let length = parseInt(prompt('Enter length:'));
+let areaRectangle = width * length;
+let perimeterRectangle = 2 * (width + length);
+console.log(`The area of the rectangle is ${areaRectangle} and the perimeter is ${perimeterRectangle}`)
 
-// // q4
-// const pi = 3.14;
-// let radius = prompt('Enter radius:');
-// let areaCircle = pi * radius^2;
-// let circumferenceCircle = 2 * pi * radius;
-// console.log(`The area of the circle is ${areaCircle} and the circumference is ${circumferenceCircle}`);
+// q4
+const pi = 3.14;
+let radius = prompt('Enter radius:');
+let areaCircle = pi * radius^2;
+let circumferenceCircle = 2 * pi * radius;
+console.log(`The area of the circle is ${areaCircle} and the circumference is ${circumferenceCircle}`);
 
 // q5
 let y, x1, x2, y1, y2;
