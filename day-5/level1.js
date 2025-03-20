bd18f3712fb602fcfccdb7e6777c842b07bbdbd5
WindowsM16a <wuvershayne@gmail.com>
2025-03-20 22:00:44 +0000

    not completely done with any, i'm tired so we pick up tomorrow

---
+++ day-5/level1.js
@@ -0,0 +1,88 @@
+// q1
+// const emptyArr = []
+
+// q2
+// const fiveArr = [1, 2, 3, 4, 5]
+
+// q3
+// console.log(fiveArr.length);
+
+// q4
+// const firstItem = fiveArr[0]
+// console.log(firstItem);
+
+// const middleItem = fiveArr[Math.floor(fiveArr.length / 2)]
+// console.log(middleItem);
+
+// const lastItem = fiveArr[fiveArr.length - 1]
+// console.log(lastItem);
+
+// q5
+const mixedDataTypes = [1, 'cars', true, 23, ['games', 'sim racing'], 'boobs' ]
+console.log(mixedDataTypes.length);
+
+// q6
+const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+// q7
+console.log(itCompanies);
+
+// q8
+console.log(itCompanies.length);
+
+// q9
+const firstComp = itCompanies[0]
+console.log(firstComp);
+
+const middleComp = itCompanies[Math.floor(itCompanies.length / 2)]
+console.log(middleComp);
+
+const lastComp = itCompanies[itCompanies.length - 1]
+console.log(lastComp);
+
+// q10
+console.log(itCompanies.join(', '));
+
+// q11
+const uppercaseItCompanies = itCompanies.map(itCompany => itCompany.toUpperCase())
+console.log(uppercaseItCompanies);
+
+// q12
+console.log(itCompanies.join(', '), 'are big IT companies');
+
+// q13
+if (itCompanies.includes('Facebook')){
+    console.log('Facebook');
+} else console.log(`Facebook not found`);
+
+// q14
+
+
+// q15
+const sortedItCompanies = itCompanies.sort()
+
+
+// q16
+console.log(itCompanies.reverse());
+
+// q17
+console.log(itCompanies.slice(0, 3));
+
+// q18
+console.log(itCompanies.slice(itCompanies.length - 3, 7));
+
+// q19
+// this wasn't easy to figure out, makes me 100% there's an easier way
+console.log(itCompanies.slice(Math.floor(itCompanies.length/2), Math.floor((itCompanies.length/2) + 1))); 
+
+// q20
+itCompanies.shift()
+console.log(itCompanies);
+
+// q21
+itCompanies.pop()
+console.log(itCompanies);
+
+// q22
+itCompanies.splice(0, itCompanies.length)
+console.log(itCompanies);
---
+++ day-5/level3.js
@@ -0,0 +1,22 @@
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+const agesSorted = ages.sort()
+
+const minAge = ages[0]
+console.log(minAge);
+
+const maxAge = ages[ages.length-1]
+console.log(maxAge);
+
+// median
+
+
+// avg age
+const avgAge = ''
+
+
+// q2
+const midCountry = countries[Math.floor(countries.length/2)]
+console.log(midCountry);
+
+// q3
