92bf596d5ae14a77a01bdfd4311fe7c7f200873a
WindowsM16a <wuvershayne@gmail.com>
2025-03-24 18:25:28 +0000

    still not done with this, day 2 complete, lvl 1 has 1 qq remaining and lvl 3 has 2 remaining. day 5 is hardish

--- day-5/level2.js
+++ day-5/level2.js
@@ -1,12 +1,48 @@
+// q2
 let text =
-'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+	"I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
 
-const noPunctText = text.replace(/[.]/g, '');
+const noPunctText = text.replace(/[.]/g, "");
 console.log(noPunctText);
 
-const textArr = noPunctText.split(' ')
+const textArr = noPunctText.split(" ");
 
-console.log(textArr)
-console.log(textArr.length)
+console.log(textArr);
+console.log(textArr.length);
 
+// q3
+const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
+shoppingCart.unshift("Meat");
+console.log(shoppingCart);
+shoppingCart.push("Sugar");
+console.log(shoppingCart);
 
+// note: works but not allergic to honey lol
+// let honeyIndex = shoppingCart.indexOf("Honey");
+// if (honeyIndex > -1) {
+// 	shoppingCart.splice(honeyIndex, 1);
+// }
+// console.log(shoppingCart);
+
+let teaIndex = shoppingCart.indexOf("Tea");
+if (teaIndex > -1) {
+	shoppingCart.splice(teaIndex, 1, "Green Tea");
+}
+console.log(shoppingCart);
+
+// q4
+if (countries.includes("Ethiopia")) {
+	console.log("ETHIOPIA");
+} else countries.push("Ethiopia");
+
+// q5
+if (webTechs.includes("Sass")) {
+	console.log("Sass is a CSS preprocessor");
+} else webTechs.push("Sass");
+console.log(webTechs);
+
+// q6
+const frontEnd = ["HTML", "CSS", "JS", "React", "Redux"];
+const backEnd = ["Node", "Express", "MongoDB"];
+const fullStack = frontEnd.concat(backEnd);
+console.log(fullStack);
--- day-5/level3.js
+++ day-5/level3.js
@@ -1,22 +1,34 @@
-const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
 
-const agesSorted = ages.sort()
+const agesSorted = ages.sort();
 
-const minAge = ages[0]
+const minAge = ages[0];
 console.log(minAge);
 
-const maxAge = ages[ages.length-1]
+const maxAge = ages[ages.length - 1];
 console.log(maxAge);
 
 // median
 
-
 // avg age
-const avgAge = ''
+let total = 0;
+for (let i = 0; i < ages.length; i++) {
+	total += ages[i];
+}
+const avg = total / ages.length;
+console.log(avg);
 
+// range
+const range = maxAge - minAge;
+console.log(range);
+
+console.log(Math.abs(minAge - avg).toFixed(2));
+console.log(Math.abs(maxAge - avg).toFixed(2));
 
 // q2
-const midCountry = countries[Math.floor(countries.length/2)]
+console.log(countries.slice(0, 10));
+const midCountry = countries[Math.floor(countries.length / 2)];
 console.log(midCountry);
 
 // q3
+console.log(countries.length);
