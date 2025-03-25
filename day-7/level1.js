8d90f5626cfc97b0a4a83efa55c04d6f84c72cc8
WindowsM16a <wuvershayne@gmail.com>
2025-03-25 22:01:38 +0000

    started day 7 but bae calls so gn

---
+++ day-7/level1.js
@@ -0,0 +1,25 @@
+// q1
+// function fullName() {
+// 	console.log("Shayne");
+// }
+// fullName();
+
+// q2
+function fullName(firstName, lastName) {
+	console.log(firstName + " " + lastName);
+}
+fullName("Shayne", "Wuver");
+
+// q3
+function addNumbers(num1, num2) {
+	const sum = num1 + num2;
+	return sum;
+}
+console.log(addNumbers(2, 3));
+
+// q4
+function areaOfRectangle(length, width) {
+	const area = length * width;
+	return area;
+}
+console.log(areaOfRectangle(2, 3));
