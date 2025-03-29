fbbc8ee996a7f79cc3f429f96166f484c6a6c8ad
WindowsM16a <wuvershayne@gmail.com>
2025-03-29 20:31:31 +0000

    learning functions

--- day-7/level1.js
+++ day-7/level1.js
@@ -1,3 +1,4 @@
+const pi = 3.14;
 // q1
 // function fullName() {
 // 	console.log("Shayne");
@@ -5,21 +6,95 @@
 // fullName();
 
 // q2
-function fullName(firstName, lastName) {
-	console.log(firstName + " " + lastName);
-}
-fullName("Shayne", "Wuver");
+// function fullName(firstName, lastName) {
+// 	console.log(`${firstName} ${lastName}`);
+// }
+// fullName("Shayne", "Wuver");
 
 // q3
-function addNumbers(num1, num2) {
-	const sum = num1 + num2;
-	return sum;
-}
-console.log(addNumbers(2, 3));
+// function addNumbers(num1, num2) {
+// 	const sum = num1 + num2;
+// 	return sum;
+// }
+// console.log(addNumbers(2, 3));
 
 // q4
-function areaOfRectangle(length, width) {
-	const area = length * width;
-	return area;
+// function areaOfRectangle(length, width) {
+// 	const area = length * width;
+// 	return area;
+// }
+// console.log(areaOfRectangle(2, 3));
+
+// q5
+// function perimeterOfRectangle(length, width) {
+// 	const perimeter = 2 * (length + width);
+// 	return perimeter;
+// }
+// console.log(perimeterOfRectangle(2, 3));
+
+// q6
+// function volumeOfPrism(length, width, height) {
+// 	const volume = length * width * height;
+// 	return volume;
+// }
+// console.log(volumeOfPrism(2, 2, 2));
+
+// q7
+// function areaOfCircle(radius) {
+// 	const area = pi * radius ** 2;
+// 	return area;
+// }
+// console.log(areaOfCircle(2));
+
+// q8
+// function circumOfCircle(radius) {
+// 	const circumference = 2 * pi * radius;
+// 	return circumference;
+// }
+// console.log(circumOfCircle(2));
+
+// q9
+// function density(mass, volume) {
+// 	const density = mass / volume;
+// 	return density;
+// }
+// console.log(density(150, 4));
+
+// q10
+// function speed(distance, time) {
+//     const speed = distance / time
+//     return speed
+// }
+// console.log(speed(100,2));
+
+// q11
+// const gravity = 6.67 * 10 ** -11;
+// function weight(mass) {
+// 	const weight = mass * gravity;
+// 	return weight / 10 ** -11;
+// }
+// console.log(`${weight(150)} x 10 ^ -11`);
+
+// q12
+// function convertCelsiusToFahrenheit(celsius) {
+// 	const fahrenheit = (celsius * 9) / 5 + 32;
+// 	return fahrenheit;
+// }
+// console.log(convertCelsiusToFahrenheit(100));
+
+// q13
+function calcBMI(weight, height) {
+	const bmi = weight / (height * height);
+	let bmiVerdict = "";
+	if (bmi <= 18.5) {
+		bmiVerdict = "Underweight";
+	} else if (bmi <= 24.9) {
+		bmiVerdict = "Normal Weight";
+	} else if (bmi <= 29.9) {
+		bmiVerdict = "Overweight";
+	} else if (bmi >= 30) {
+		bmiVerdict = "Obese";
+	}
+	return `${bmiVerdict}: BMI is ${bmi}`;
 }
-console.log(areaOfRectangle(2, 3));
+console.log(calcBMI(110, 1.9));
